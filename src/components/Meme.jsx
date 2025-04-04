4f816a93a9cec24c8d8c9b2712bc73ff081d8975
Donnesca <163658888+Donnesca@users.noreply.github.com>
2025-04-04 23:58:03 +0200

    Implemented Meme component with state management and added functionality to fetch meme images from API.

---
+++ src/components/Meme.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import memesData from "../memesData.jsx";
+
+export default function Meme() {
+  //const [memeImage, setMemeImage] = React.useState("");
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input type="text" placeholder="Top text" className="form--input" />
+        </label>
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+          />
+        </label>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={meme.randomImage} className="meme--image" />
+    </main>
+  );
+}
